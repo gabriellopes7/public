18e5082715a39ee71ba648558d134272d24b9d1a
Gabriel Lopes da Silva Ladeira <gabriellopes7@yahoo.com.br>
2019-12-06 19:37:38 -0300

    aed

---
+++ exercicios/aed/Pratica 6 Arvore Binaria/Pratica6/Pratica6/Arvore.cs
@@ -0,0 +1,108 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pratica6
+{
+    class Arvore
+    {
+        public NoArvore A = null;
+
+        public void inserir(string chave, string[] aluno)
+        {
+            if (A == null)
+            {
+                A = new NoArvore(chave, aluno);
+            }
+            else
+            {
+                A.inserir(chave, aluno);
+            }
+        }
+
+        public void emOrdem(NoArvore x)
+        {
+            if (x != null)
+            {
+                emOrdem(x.esq);
+                Console.Write(x.chave + " ");
+                Console.WriteLine();
+                emOrdem(x.dir);
+            }
+        }
+
+        public void preOrdem(NoArvore x)
+        {
+            if (x != null)
+            {
+                Console.Write(x.chave + " ");
+                Console.WriteLine();
+                preOrdem(x.esq);
+                preOrdem(x.dir);
+            }
+        }
+
+        public void posOrdem(NoArvore x)
+        {
+            if (x != null)
+            {
+                posOrdem(x.esq);
+                posOrdem(x.dir);
+                Console.Write(x.chave + " ");
+                Console.WriteLine();
+            }
+        }
+
+        public NoArvore pesquisar(string c, NoArvore d)
+        {
+            if (d == null || d.chave == c)
+                return d;
+            else if (string.Compare(c, d.chave) == -1)
+                return pesquisar(c, d.esq);
+            else
+                return pesquisar(c, d.dir);
+        }
+    }
+
+    public class NoArvore
+    {
+        public string chave;
+        public List<string[]> listaAluno = new List<string[]>();
+        public NoArvore esq, dir;
+
+        public NoArvore(string c, string[] aluno)
+        {
+            chave = c;
+            esq = dir = null;
+            listaAluno.Add(aluno);
+        }
+
+        public void inserir(string c, string[] aluno)
+        {
+            if (string.Compare(c, chave) == -1)
+            {
+                if (esq == null)
+                    esq = new NoArvore(c, aluno);
+                else
+                    esq.inserir(c, aluno);
+            }
+            else if (string.Compare(c, chave) == 1)
+            {
+                if (dir == null)
+                    dir = new NoArvore(c, aluno);
+                else
+                    dir.inserir(c, aluno);
+            }
+            else
+            {
+                if(!listaAluno.Contains(aluno))
+                    listaAluno.Add(aluno);
+                //else
+                //Console.Write("Chave duplicada. Impossível inserir!");
+            }
+        }
+    }
+}
---
+++ exercicios/aed/Pratica 6 Arvore Binaria/Pratica6/Pratica6/Program.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Pratica6
+{
+
+    class Program
+    {
+        static Arvore arvore = new Arvore();
+        static string Arquivo = "";
+
+        [STAThread]
+        static void Main(string[] args)
+        {
+            string line;
+            OpenFileDialog theDialog = new OpenFileDialog();
+            theDialog.Title = "Abrir arquivo de texto";
+            theDialog.Filter = "TXT files|*.txt";
+            theDialog.InitialDirectory = @"h:\";
+            if (theDialog.ShowDialog() == DialogResult.OK)
+            {
+                System.IO.StreamReader file = new System.IO.StreamReader(theDialog.FileName);
+                Arquivo = new System.IO.FileInfo(theDialog.FileName).Name;
+
+                while ((line = file.ReadLine()) != null)
+                {
+                    string[] s = line.Split(';');
+
+                    foreach (var materia in s.Select((x,i) => new {Value = x, Index = i }))
+                    {
+                        if(materia.Index > 1)
+                        {
+                            arvore.inserir(s[materia.Index], s);
+                        }
+                    }
+                }
+
+                Menu();
+
+                file.Close();
+            }
+
+            Console.ReadLine();
+        }
+
+
+
+        static void OpcoesMenu()
+        {
+            Console.Clear();
+            Console.WriteLine("F1  -  Informar arquivo");
+            Console.WriteLine("F2  -  Imprimir disciplinas em ordem crescente");
+            Console.WriteLine("F3  -  Imprimir disciplinas em pré-ordem");
+            Console.WriteLine("F4  -  Imprimir disciplinas em pós - ordem");
+            Console.WriteLine("F5  -  Listar Turma");
+            Console.WriteLine("ESC -  Sair");
+        }
+
+        static void Menu()
+        {
+            OpcoesMenu();
+
+            ConsoleKeyInfo opcao;
+            opcao = Console.ReadKey();
+            do
+            {
+
+
+                switch (opcao.Key)
+                {
+                    case ConsoleKey.F1:
+                        {
+                            Console.Clear();
+                            Console.WriteLine(Arquivo);
+                            Console.ReadKey();
+                            OpcoesMenu();
+                            Menu();
+                        }
+                        break;
+                    case ConsoleKey.F2:
+                        {
+                            Console.Clear();
+                            arvore.emOrdem(arvore.A);
+                            Console.ReadKey();
+                            OpcoesMenu();
+                            Menu();
+                        }
+                        break;
+                    case ConsoleKey.F3:
+                        {
+                            Console.Clear();
+                            arvore.preOrdem(arvore.A);
+                            Console.ReadKey();
+                            OpcoesMenu();
+                            Menu();
+                        }
+                        break;
+                    case ConsoleKey.F4:
+                        {
+                            Console.Clear();
+                            arvore.posOrdem(arvore.A);
+                            Console.ReadKey();
+                            OpcoesMenu();
+                            Menu();
+                        }
+                        break;
+                    case ConsoleKey.F5:
+                        {
+                            Console.Clear();
+
+                            Console.Write("Informe o nome da disciplina: ");
+                            string materia = Console.ReadLine();
+
+                            Console.WriteLine();
+                            Console.WriteLine("ALUNOS MATRICULADOS:");
+                            NoArvore result = arvore.pesquisar(materia.ToUpper(), arvore.A);
+
+                            foreach (var s in result.listaAluno)
+                            {
+                                Console.WriteLine($"{s[0]} - {s[1]}");
+                            }
+                            Console.ReadKey();
+                            OpcoesMenu();
+                            Menu();
+                        }
+                        break;
+                    case ConsoleKey.Escape:
+                        {
+                            Environment.Exit(0);
+                        }
+                        break;
+                }
+            } while (opcao.Key != ConsoleKey.F1 && opcao.Key != ConsoleKey.F2 && opcao.Key != ConsoleKey.F3 && opcao.Key != ConsoleKey.F4 && opcao.Key != ConsoleKey.F5 && opcao.Key != ConsoleKey.Escape);
+        }
+
+
+    }
+}
